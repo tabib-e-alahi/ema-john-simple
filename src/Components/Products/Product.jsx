4354cbbf976dddacc07bc4545be63f5365fa5f07
tabib-e-alahi <etabib.alahi@gmail.com>
2023-04-05 01:17:10 +0600

    added cart style

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const {name,seller,price,stock,ratings,img}=props.product;
+    const { name, seller, price, stock, ratings, img } = props.product;
     return (
         <div className='product'>
             <img src={img} alt='' />
-            <h6>{name}</h6>
+            <div className="product-info">
+
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>Rating: {ratings} stars</p>
+
+            </div>
+
+            <button className='btn-cart'>Add to cart</button>
         </div>
     );
 };
